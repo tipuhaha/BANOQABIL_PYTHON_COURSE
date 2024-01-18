431283ad1ca723794acc615dee27cece3b665a63
tipuhaha <156343470+tipuhaha@users.noreply.github.com>
2024-01-18 21:46:05 +0500

    Add files via upload

---
+++ tipu_sultan_midterm.py
@@ -0,0 +1,40 @@
+#Muhammad Tipu sultan
+
+#midterm exam
+
+#Q1 - Write a Python program to do arithmetical operations addition and division.
+
+a=int(input("enter number1"))
+b=int(input("enter number2"))
+sum= a+b
+print("the sum is:" ,sum)
+
+c=int(input("numerator"))
+d=int(input("denominator"))
+answer = c/d
+print("the answer is:" , answer)
+
+#Q2 - Write a Python program to find the area of a triangle
+
+base= int(input("base of triangle"))
+height=int(input("height of triangle"))
+area= base*height*0.5
+print("then area of triangle is:", area)
+
+#Q3 - Write a Python program to convert Celsius to Fahrenheit.
+
+celsius= 30
+farenheit= celsius*9/5 + 32
+print("conversion:", farenheit)
+
+#Q4 - Write a Python program that return all datatypes that we discussed in the class.
+
+integer=55
+float=55.0
+string= "tipu"
+boolean= 5==5
+
+print("the data type of ", integer ,"is:", type(integer))
+print("the data type of ",float," is:", type(float))
+print("the data type of ",string," is :",type(string))
+print("the data type of", boolean," is :", type(boolean))
